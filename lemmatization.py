ffe4c3d5f3aa1a71fac38944b7accd750e612c25
SvetlanaYes <yesayan.svetlana@student.rau.am>
2022-07-19 17:24:50 +0400

    add discriptions of functions and make some corrections

--- lemmatization.py
+++ lemmatization.py
@@ -3,11 +3,17 @@ import json
 import os
 from configparser import ConfigParser
 import sys
+import utils as u
 
-'''
-Discription:
-This code lemmatizes train texts, and adds lemma keys to train json files
-'''
+"""
+    Args:
+         stopwords: Armenian stopwords path
+         filenames: File with train/test filenames(json)
+    Discription:
+    This code lemmatizes texts with stanza.Pipeline, removes stopwords from the texts and adds lemma keys to json files
+    
+"""
+
 
 file = 'config.ini'
 config = ConfigParser()
@@ -17,16 +23,36 @@ nlp = stanza.Pipeline('hy', tokenize_pretokenized=True, processors='tokenize,lem
 
 
 def get_lemmas(text, stopwords):
-    a = []
+    """
+
+    Args:
+        text: original text loaded from json file
+        stopwords: Armenian stopwords path
+
+    Returns: list with lemmatized words of text, without stopwords
+
+    """
+    lemmatized_no_stopwords_text = []
     processed_text = nlp(text)
     for s in processed_text.sentences:
         for w in s.words:
             if w.lemma not in stopwords:
-               a.append(w.lemma)
-    return a
+               lemmatized_no_stopwords_text.append(w.lemma)
+    return lemmatized_no_stopwords_text
 
 
 def add_lemma_key(file_path, stopwords):
+    """
+
+    Args:
+        file_path: absolute path of file containing text
+        stopwords: Armenian stopwords path
+
+    Json files have 2 keys
+    'src' - text
+    'tgt' - title of the text
+     This function lemmatizes text and adds 'lemma' key
+    """
     with open(file_path, 'r') as c:
         content_of_file = json.load(c)
     if 'lemma' in content_of_file.keys():
@@ -37,29 +63,17 @@ def add_lemma_key(file_path, stopwords):
         json.dump(content_of_file, json_file, ensure_ascii=False)
 
 
-def read_file(file):
-    with open(file, 'r') as s:
-        content = s.readlines()
-    return content
-
-
-def process_stopwords(stopwords):
-    for i in range(len(stopwords)):
-        stopwords[i] = stopwords[i].strip()
-    return stopwords
-
-
 def main():
     if len(sys.argv) != 3:
-        print("Specify correct arguments! \n[stopwords] [train filenames]")
+        print("Specify correct arguments! \n[stopwords] [filenames]")
         return
-    stopwords = read_file(sys.argv[1])
-    stopwords = process_stopwords(stopwords)
-    train_filenames = read_file(sys.argv[2])
+    stopwords = u.get_filenames(sys.argv[1])
+    stripped_stopwords = [s.strip() for s in stopwords]
+    train_filenames = u.get_filenames(sys.argv[2])
     for file in train_filenames:
         file_path = config['folder_names'][file[0]] + file.strip()
         if os.path.exists(file_path):
-            add_lemma_key(file_path, stopwords)
+            add_lemma_key(file_path, stripped_stopwords)
 
 
 if __name__ == "__main__":
